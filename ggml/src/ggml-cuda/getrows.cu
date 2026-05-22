f8cc5007f13159aa7e48318e030d11753850c874
THEman6989 <aminey2004@gmail.com>
2026-05-22 23:14:01 +0200

    Audit & Fix: Deactivate incomplete TurboQuant HIP paths in fattn and getrows; all other optimizations preserved

--- ggml/src/ggml-cuda/getrows.cu
+++ ggml/src/ggml-cuda/getrows.cu
@@ -220,16 +220,9 @@ static void ggml_cuda_get_rows_switch_src0_type(
                 ne00, nb01, nb02, nb03, ne10, ne11, ne12, nb10, nb11, nb12, nb1, nb2, nb3, stream);
             break;
         case GGML_TYPE_TURBO3_0:
-            get_rows_cuda_turbo3(src0_d, src1_d, dst_d,
-                ne00, nb01, nb02, nb03, ne10, ne11, ne12, nb10, nb11, nb12, nb1, nb2, nb3, stream);
-            break;
         case GGML_TYPE_TURBO4_0:
-            get_rows_cuda_turbo4(src0_d, src1_d, dst_d,
-                ne00, nb01, nb02, nb03, ne10, ne11, ne12, nb10, nb11, nb12, nb1, nb2, nb3, stream);
-            break;
         case GGML_TYPE_TURBO2_0:
-            get_rows_cuda_turbo2(src0_d, src1_d, dst_d,
-                ne00, nb01, nb02, nb03, ne10, ne11, ne12, nb10, nb11, nb12, nb1, nb2, nb3, stream);
+            GGML_ABORT("%s: turbo get_rows CUDA path not implemented for type: %s\n", __func__, ggml_type_name(src0_type));
             break;
         default:
             // TODO: k-quants
